095f87ac42db1498b412d358ef3dc1cc11a2064e
Гасан <hasanaliyeev@gmail.com>
2022-10-31 00:59:36 +0300

    first commit

---
+++ travel/views.py
@@ -0,0 +1,8 @@
+import datetime
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+dt = datetime.date.today()
+def home(request):
+    return render(request, 'home.html', {'date':dt})
