eebacd7ef4e0a31135e89acd0a962cc2bcca68d6
Гасан <hasanaliyeev@gmail.com>
2022-11-04 00:59:15 +0300

    created CreateView class

--- cities/models.py
+++ cities/models.py
@@ -1,6 +1,7 @@
 import datetime
 
 from django.db import models
+from django.urls import reverse
 
 
 class City(models.Model):
@@ -13,6 +14,9 @@ class City(models.Model):
         verbose_name = 'Город'
         verbose_name_plural = 'Города'
         ordering = ['name']
+
+    def get_absolute_url(self):
+        return reverse('cities:detail', kwargs={'pk':self.pk})
 
 
 class Person(models.Model):
